39b530ea161a7e71cb2a351833728b645bde7e0b
diogocesar17 <diogocesar17@gmail.com>
2025-07-17 12:06:25 -0300

    teste deploy automatico

--- index.js
+++ index.js
@@ -769,7 +769,6 @@ async function startBot() {
     // Persistência local de sessão WhatsApp
     const { state, saveCreds } = await useMultiFileAuthState('auth');
 
-    
   const sock = makeWASocket({
     auth: state,
     printQRInTerminal: false,
